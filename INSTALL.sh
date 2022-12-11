25756ef5ecff45df08b2ad73c6ce67444b3b9464
MertGunduz <merttgg@gmail.com>
2022-12-11 12:10:34 +0300

    installation file created

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,18 +1,76 @@
 #!/bin/bash
-DIR=build
 
+#****************************************
+# BUILD DIRECTORY CREATION PART 
+#****************************************
 if [ ! -d "build" ]
 then
     mkdir build
 fi
 
-cmake -S . -B build
+echo "=%= build directory created =%="
 
-# goes to the build directory and runs make
-cd ./build && make
 
-# goes to the build/src/app folder and moves the mv folder to usr/bin (exexcutable place)
-cd src/app/ && sudo mv wb /usr/bin
+#****************************************
+# USR-DIRECTORY CONTROL PART 
+#****************************************
+if [ -d "usr/local/wb" ] && [ -f "usr/bin/wb" ] 
+then
+    # delete the files if they are exist in the system
+    sudo rm -r /usr/local/wb
+    sudo rm /usr/bin/wb
+fi
 
-# creates a directory in usr/local
-sudo mkdir /usr/local/wb
+echo "=%= file structure controlling =%="
+sleep 5
+
+#****************************************
+# COMPILING AND LINKING PART 
+#****************************************
+if [ -d "build" ]
+then
+    # run cmake and go to build directory then run makefiles
+    cmake -S . -B build
+    cd build && make
+fi
+
+echo "=%= compiling and linking the project =%="
+sleep 5
+
+#****************************************
+# MOVING THE EXECUTABLE TO USR/BIN PART
+#****************************************
+if [ ! -f "/usr/bin/wb" ]
+then
+    # move the wb executable to usr/bin (exexcutable place)
+    sudo mv src/app/wb /usr/bin
+fi
+
+echo "=%= moving the executable to usr/bin =%="
+sleep 5
+
+#****************************************
+# CREATING WB FOLDER TO USR/LOCAL PART
+#****************************************
+if [ ! -d "/usr/local/wb" ]
+then
+    sudo mkdir /usr/local/wb
+fi
+
+echo "=%= creating the wb folder to usr/local =%="
+sleep 5
+
+#****************************************
+# DELETING THE SOURCE BUILD DIRECTORY
+#****************************************
+cd .. && rm -r build
+
+echo "=%= removing the build directory =%="
+sleep 5
+
+if [ -d "/usr/local/wb" ] && [ -f "/usr/bin/wb" ]
+then 
+    sudo echo "=%= wb installation succesful =%="
+else
+    sudo echo "=%= wb installation unsuccesful =%="
+fi
