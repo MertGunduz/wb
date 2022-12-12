7d88c45a0b562e129b254019e71e32233d331fa9
MertGunduz <merttgg@gmail.com>
2022-12-12 10:27:34 +0300

    wb argument if-else added

--- src/app/wb.c
+++ src/app/wb.c
@@ -11,13 +11,37 @@
 */
 
 #include <stdio.h>
+#include <ncurses.h>
 #include "../wbgen/wbgen.h"
 
 int main(int argc, char *argv[])
 {
     if (verify(argc, argv[1]))
     {
-        
+        if (strcmp(argv[1], "-a") == 0 || strcmp(argv[1], "--add") == 0)
+        {
+            fprintf(stdout, "add menu!\n");
+        }
+        else if (strcmp(argv[1], "-e") == 0 || strcmp(argv[1], "--edit") == 0) 
+        {
+            fprintf(stdout, "edit menu!\n");
+        }
+        else if (strcmp(argv[1], "-l") == 0 || strcmp(argv[1], "--list") == 0) 
+        {
+            fprintf(stdout, "list menu!\n");
+        }
+        else if (strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) 
+        {
+            fprintf(stdout, "help menu!\n");
+        }
+        else if (strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0) 
+        {
+            fprintf(stdout, "version menu!\n");
+        }
+        else if (strcmp(argv[1], "-g") == 0 || strcmp(argv[1], "--github") == 0) 
+        {
+            fprintf(stdout, "github menu!\n");
+        }
     }
     return 0;
 }
