f5a87b0eaca175d856920c77e621deb65e3c0b11
MertGunduz <merttgg@gmail.com>
2022-12-18 18:15:46 +0300

    doxygen added

--- docs/html/search/all_9.js
+++ docs/html/search/all_9.js
@@ -2,6 +2,6 @@ var searchData=
 [
   ['terminalcolorerror_21',['terminalColorError',['../d3/d49/terminalColorError_8c.html#a7ace3022215c46cc8d5790059f4fa795',1,'terminalColorError():&#160;terminalColorError.c'],['../d5/d60/wbinfo_8h.html#a7ace3022215c46cc8d5790059f4fa795',1,'terminalColorError():&#160;terminalColorError.c']]],
   ['terminalcolorerror_2ec_22',['terminalColorError.c',['../d3/d49/terminalColorError_8c.html',1,'']]],
-  ['textinput_23',['textInput',['../d0/d94/textInput_8c.html#a763877a3a68abe401e43e6a5391a995a',1,'textInput(WINDOW *window, int ct, int wordstrlen, int winX, char *string):&#160;textInput.c'],['../d3/d23/wbgen_8h.html#a763877a3a68abe401e43e6a5391a995a',1,'textInput(WINDOW *window, int ct, int wordstrlen, int winX, char *string):&#160;textInput.c']]],
+  ['textinput_23',['textInput',['../d0/d94/textInput_8c.html#a7b17a6c72a0b183ba90901d387f1d779',1,'textInput(WINDOW *window, int ct, int wordstrlen, int winX, char *string, int locationSetter):&#160;textInput.c'],['../d3/d23/wbgen_8h.html#a7b17a6c72a0b183ba90901d387f1d779',1,'textInput(WINDOW *window, int ct, int wordstrlen, int winX, char *string, int locationSetter):&#160;textInput.c']]],
   ['textinput_2ec_24',['textInput.c',['../d0/d94/textInput_8c.html',1,'']]]
 ];
