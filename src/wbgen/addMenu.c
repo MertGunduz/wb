d66c5475ae88d928d89f52bf8c420d706d12cf82
MertGunduz <merttgg@gmail.com>
2022-12-18 08:57:55 +0300

    add menu design finished git push -u origin

--- src/wbgen/addMenu.c
+++ src/wbgen/addMenu.c
@@ -14,12 +14,12 @@
 /// @brief the word adding menu for the wb, opens a terminal ui and asks user some data to generate word files
 void addMenu()
 {
-    char word[65]; /**< word data (example: discreet)*/
-    char wordType[65]; /**< word type data (example: adjective)*/
-    char wordDesc[257]; /**< word description data (example: careful not to cause embarrassment or attract too much attention, especially by keeping something secret)*/
-    char wordExample[257]; /**< word example data (example: The family made discreet enquiries about his background)*/
-    char wordOpposite[65]; /**< word opposite data (example: indiscreet)*/
-    char wordDate[33]; /**< word add date data (example: 05/1/2023)*/
+    char word[65]; /* word data (example: discreet)*/
+    char wordType[65]; /* word type data (example: adjective)*/
+    char wordDesc[257]; /* word description data (example: careful not to cause embarrassment or attract too much attention, especially by keeping something secret)*/
+    char wordExample[257]; /* word example data (example: The family made discreet enquiries about his background)*/
+    char wordOpposite[65]; /* word opposite data (example: indiscreet)*/
+    char wordDate[33]; /* word add date data (example: 05/1/2023)*/
 
     initscr();
         
@@ -27,6 +27,9 @@ void addMenu()
     {
         start_color();
         bgColorSet();
+        init_color(COLOR_BLACK, 150, 150, 150);
+        init_pair(100, COLOR_WHITE, COLOR_BLACK);
+        init_pair(101, COLOR_BLACK, PURE_BLACK_COLOR);
     }
     else
     {
@@ -35,12 +38,13 @@ void addMenu()
         exit(0);
     }
 
+    /******************************************
+    * PANEL START
+    ******************************************/
     WINDOW *topPanel = newwin(1, COLS, 0, 0);
 
     if (topPanel != NULL)
     {
-        init_pair(100, PURE_BLACK_COLOR, COLOR_WHITE);
-
         wbkgd(topPanel, COLOR_PAIR(100));
 
         /* date & time */
@@ -57,13 +61,145 @@ void addMenu()
         /* refresh the top panel */
         refresh();
         wrefresh(topPanel);
-
-        /* curs set 0 */
-        curs_set(0);
     } 
+
+    /******************************************
+    * WORD WINDOW START
+    ******************************************/
+   WINDOW *wordWindow = newwin(3, COLS / 2 - 1, 2, 0);
+
+   if (wordWindow != NULL)
+   {
+        wbkgd(wordWindow, COLOR_PAIR(100));
+        
+        mvwprintw(wordWindow, 1, 1, "WORD: ");
+
+        /* move to input textbox */
+        //wmove(wordWindow, 1, 7);
+
+        /* refresh the word window */
+        refresh();
+        wrefresh(wordWindow);
+
+        /* take input */
+        //wgetnstr(wordWindow, word, 64);
+    }
+    
+    /******************************************
+    * WORD TYPE WINDOW START
+    ******************************************/
+    WINDOW *wordTypeWindow = newwin(3, COLS / 2 , 2, COLS / 2 + 1);
+
+    if (wordTypeWindow != NULL)
+    {
+        wbkgd(wordTypeWindow, COLOR_PAIR(100));
+        
+        mvwprintw(wordTypeWindow, 1, 1, "WORD TYPE: ");
+
+        /* move to input textbox */
+        //wmove(wordTypeWindow, 1, 7);
+
+        /* refresh the word window */
+        refresh();
+        wrefresh(wordTypeWindow);
+
+        /* take input */
+        //wgetnstr(wordTypeWindow, word, 64);
+    }
+
+    /******************************************
+    * WORD OPPOSITE WINDOW START
+    ******************************************/
+    WINDOW *oppositeWordWindow = newwin(3, COLS, 6, 0);
+
+    if (oppositeWordWindow != NULL)
+    {
+        wbkgd(oppositeWordWindow, COLOR_PAIR(100));
+        
+        mvwprintw(oppositeWordWindow, 1, 1, "WORD OPPOSITE: ");
+
+        /* move to input textbox */
+        //wmove(oppositeWordWindow, 1, 7);
+
+        /* refresh the word window */
+        refresh();
+        wrefresh(oppositeWordWindow);
+
+        /* take input */
+        //wgetnstr(oppositeWordWindow, word, 64);
+    }
+
+    /******************************************
+    * WORD OPPOSITE WINDOW START
+    ******************************************/
+    WINDOW *wordDescWindow = newwin(3, COLS, 10, 0);
+
+    if (wordDescWindow != NULL)
+    {
+        wbkgd(wordDescWindow, COLOR_PAIR(100));
+        
+        mvwprintw(wordDescWindow, 1, 1, "WORD DESCRIPTION: ");
+
+        /* move to input textbox */
+        //wmove(wordDescWindow, 1, 7);
+
+        /* refresh the word window */
+        refresh();
+        wrefresh(wordDescWindow);
+
+        /* take input */
+        //wgetnstr(wordDescWindow, word, 64);
+    }
+
+    /******************************************
+    * WORD EXAMPLE WINDOW START
+    ******************************************/
+    WINDOW *wordExampleWindow = newwin(3, COLS, 14, 0);
+
+    if (wordExampleWindow != NULL)
+    {
+        wbkgd(wordExampleWindow, COLOR_PAIR(100));
+        
+        mvwprintw(wordExampleWindow, 1, 1, "WORD EXAMPLE: ");
+
+        /* move to input textbox */
+        //wmove(wordExampleWindow, 1, 7);
+
+        /* refresh the word window */
+        refresh();
+        wrefresh(wordExampleWindow);
+
+        /* take input */
+        //wgetnstr(wordExampleWindow, word, 64);
+    }
+
+    /******************************************
+    * BACKGROUND PATTERN DESIGN
+    ******************************************/
+    attron(COLOR_PAIR(101));
+
+    for (int i = 0; i < COLS; i++)
+    {
+        mvprintw(17, i, "_");
+    }
+
+    for (int i = 18; i < LINES; i++)
+    {
+        for (int j = 0; j < COLS; j=j+3)
+        {
+            mvprintw(i, j, "wb ");
+        }
+    }
+
+    curs_set(0);
 
     getch();
 
     delwin(topPanel);
+    delwin(wordWindow);
+    delwin(wordTypeWindow);
+    delwin(oppositeWordWindow);
+    delwin(wordDescWindow);
+    delwin(wordExampleWindow);
     endwin();
 }
