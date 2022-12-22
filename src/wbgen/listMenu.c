7c320fa8e87496535eca640b014e820c12ff97c8
MertGunduz <merttgg@gmail.com>
2022-12-23 02:51:59 +0300

    list menu general structure coded

--- src/wbgen/listMenu.c
+++ src/wbgen/listMenu.c
@@ -10,10 +10,14 @@
 
 #include "wbgen.h"
 
+void spaceAdder(WINDOW *window, int spacer);
+
 /// @brief the main function for displaying the words
 void listMenu()
 {
     char wordDate[33]; /* word add date data (example: 05/1/2023)*/
+    int spacer;
+    char travelInput;
 
     initscr();
     noecho();
@@ -50,7 +54,7 @@ void listMenu()
         sprintf(wordDate, "%02d/%02d/%04d", tm.tm_mday, tm.tm_mon + 1, tm.tm_year + 1900);
 
         /* top panel information printing */
-        mvwprintw(topPanel, 0, 1, "ADD MENU");
+        mvwprintw(topPanel, 0, 1, "LIST MENU");
         mvwprintw(topPanel, 0, (COLS - 2) / 2, "wb");
         mvwprintw(topPanel, 0, COLS - strlen(wordDate) - 1, "%s", wordDate);
        
@@ -58,6 +62,134 @@ void listMenu()
         fullrefresh(topPanel);
     } 
 
+    /******************************************
+    * LIST TOP DESCRIPTION PANEL START
+    ******************************************/
+    WINDOW *listTopPanel = newwin(1, COLS, 2, 0); // WORD - TYPE - OPPOSITE - DESCRIPTION - EXAMPLE - DATE  
+
+    if (listTopPanel != NULL)
+    {        
+        wbkgd(listTopPanel, COLOR_PAIR(100));
+
+        waddch(listTopPanel, ' ');
+
+        if (COLS >= 43)
+        {
+
+            spacer = COLS - 43;
+            spacer = spacer / 4;
+
+            wprintw(listTopPanel,  "WORD");
+            spaceAdder(listTopPanel, spacer);
+
+            wprintw(listTopPanel,  "TYPE");
+            spaceAdder(listTopPanel, spacer);
+            
+            wprintw(listTopPanel,  "OPPOSITE");
+            spaceAdder(listTopPanel, spacer);
+
+            wprintw(listTopPanel,  "DESCRIPTION");
+            spaceAdder(listTopPanel, spacer);
+            
+            wprintw(listTopPanel,  "EXAMPLE");
+            spaceAdder(listTopPanel, spacer);
+                
+            wprintw(listTopPanel,  "DATE");
+            spaceAdder(listTopPanel, spacer);
+        }
+        else
+        {
+            spacer = COLS - 35;
+            spacer = spacer / 4;
+
+            wprintw(listTopPanel,  "WORD");
+            spaceAdder(listTopPanel, spacer);
+
+            wprintw(listTopPanel,  "TYPE");
+            spaceAdder(listTopPanel, spacer);
+            
+            wprintw(listTopPanel,  "OPP...");
+            spaceAdder(listTopPanel, spacer);
+
+            wprintw(listTopPanel,  "DESC...");
+            spaceAdder(listTopPanel, spacer);
+            
+            wprintw(listTopPanel,  "EX...");
+            spaceAdder(listTopPanel, spacer);
+                
+            wprintw(listTopPanel,  "DATE");
+            spaceAdder(listTopPanel, spacer);
+        }
+
+        fullrefresh(listTopPanel);
+    }
+
+    WINDOW *listPanel;
+
+    if (LINES >= 19)
+    {
+        listPanel = newwin(15, COLS, 3, 0);
+    }
+    else
+    {
+        listPanel = newwin(LINES - 5, COLS, 3, 0);
+    }
+
+    if (listPanel != NULL)
+    {
+        wbkgd(listPanel, COLOR_PAIR(100));
+
+        fullrefresh(listPanel);
+
+        if (LINES >= 19)
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                mvwprintw(listPanel, i, 1, "%d", i);
+            }
+        }
+        else
+        {
+            for (int i = 0; i < LINES - 5; i++)
+            {
+                mvwprintw(listPanel, i, 1, "%d", i);
+            }
+        }
+
+        fullrefresh(listPanel);
+    }
+
+    int lengthOfList = getmaxy(listPanel);
+
+    WINDOW *useWindow = newwin(1, COLS, lengthOfList + 4, 0);
+
+    if (useWindow != NULL)
+    {
+        wbkgd(useWindow, COLOR_PAIR(100));
+
+        fullrefresh(useWindow);
+    }
+
+    do
+    {
+        travelInput = getch();
+    } while (travelInput != 'q');
+    
     getch();
     endwin();
+}
+
+void spaceAdder(WINDOW *window, int spacer)
+{
+    if (spacer > 0)
+    {
+        for (int i = 0; i < spacer; i++)
+        {
+            waddch(window, ' ');
+        }
+    }
+    else
+    {
+        waddch(window, ' ');
+    }
 }
