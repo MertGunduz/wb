f139970710d4677cd35b975c80b52113d2cf49c3
MertGunduz <merttgg@gmail.com>
2022-12-23 12:33:55 +0300

    version message update

--- src/wbinfo/versionMessage.c
+++ src/wbinfo/versionMessage.c
@@ -18,5 +18,5 @@ void versionMessage()
     fprintf(stdout, "license GPL 3.0: GNU GPL version 3 %shttps://gnu.org/licenses/gpl.html%s\n", KMAG, KNRM);
     fprintf(stdout, "this is a free software: you are free to change and redistribute it.\n");
     fprintf(stdout, "there is no warranty, to the extent permitted by law.\n\n");
-    fprintf(stdout, "written by Mehmet Mert Gunduz and others.\n");
+    fprintf(stdout, "developed by Mehmet Mert Gunduz and others.\n");
 }
