9e63ce398067ed075a1069e1768dc6c4bb1aa127
MertGunduz <merttgg@gmail.com>
2023-01-13 18:08:41 +0300

    version menu improved

--- src/wbinfo/versionMessage.c
+++ src/wbinfo/versionMessage.c
@@ -14,7 +14,7 @@
 /// @brief prints the wb version
 void versionMessage()
 {
-    fprintf(stdout, "wb (wordbook) version %s1.0.0%s\n", KMAG, KNRM);
+    fprintf(stdout, "wb (wordbook) version %s1.0.0%s\n\n", KMAG, KNRM);
     fprintf(stdout, "license GPL 3.0: GNU GPL version 3 %shttps://gnu.org/licenses/gpl.html%s\n", KMAG, KNRM);
     fprintf(stdout, "this is a free software: you are free to change and redistribute it.\n");
     fprintf(stdout, "there is no warranty, to the extent permitted by law.\n\n");
